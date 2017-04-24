90aaa4e32f4faed3c1ddc074a43802b3f0ca5ad9
Pavel L <nitrolovsky@gmail.com>
2017-04-24 17:10:10 +0300

    Changed view leads

--- resources/views/leads/index.blade.php
+++ resources/views/leads/index.blade.php
@@ -3,63 +3,76 @@
     @if (count($leads) > 0)
         <div class="container">
             <div class="row">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>
-                                Id
-                            </th>
-                            <th>
-                                Дата создания
-                            </th>
-                            <th>
+                @foreach ($leads as $lead)
+                    <div class="card card-block">
+                        <div class="row py-1">
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 text-xl-right text-lg-right text-md-right text-left">
+                                Идентификатор
+                            </div>
+                            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-12">
+                                {{ $lead->id }}
+                            </div>
+                        </div>
+
+                        <div class="row py-1">
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 text-xl-right text-lg-right text-md-right text-left">
+                                Дата и время создания
+                            </div>
+                            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-12">
+                                {{ $lead->created_at }}
+                            </div>
+                        </div>
+
+                        <div class="row py-1">
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 text-xl-right text-lg-right text-md-right text-left">
                                 Источник
-                            </th>
-                            <th>
+                            </div>
+                            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-12">
+                                {{ $lead->source }}
+                            </div>
+                        </div>
+
+                        <div class="row py-1">
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 text-xl-right text-lg-right text-md-right text-left">
                                 Имя
-                            </th>
-                            <th>
+                            </div>
+                            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-12">
+                                {{ $lead->name }}
+                            </div>
+                        </div>
+
+                        <div class="row py-1">
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 text-xl-right text-lg-right text-md-right text-left">
                                 Email
-                            </th>
-                            <th>
+                            </div>
+                            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-12">
+                                {{ $lead->email }}
+                            </div>
+                        </div>
+
+                        <div class="row py-1">
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 text-xl-right text-lg-right text-md-right text-left">
                                 Телефон
-                            </th>
-                            <th>
+                            </div>
+                            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-12">
+                                {{ $lead->phone }}
+                            </div>
+                        </div>
+
+                        <div class="row py-1">
+                            <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 text-xl-right text-lg-right text-md-right text-left">
                                 Статус
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($leads as $lead)
-                            <tr>
-                                <td>
-                                    {{ $lead->id }}
-                                </td>
-                                <td>
-                                    {{ $lead->created_at }}
-                                </td>
-                                <td>
-                                    {{ $lead->source }}
-                                </td>
-                                <td>
-                                    {{ $lead->name }}
-                                </td>
-                                <td>
-                                     {{ $lead->email }}
-                                </td>
-                                <td>
-                                     {{ $lead->phone }}
-                                </td>
-                                <td>
-                                    <form method="POST" action="/leads/{{ $lead->id }}" class="form-inline">
-                                        {{ method_field('PUT') }}
-                                        {{ csrf_field() }}
-                                        <textarea class="form-control form-control-sm status" type="text" name="status" id="{{ $lead->id }}">{{ $lead->status }}</textarea>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                </table>
+                            </div>
+                            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-12">
+                                <form method="POST" action="/leads/{{ $lead->id }}" class="form-inline">
+                                    {{ method_field('PUT') }}
+                                    {{ csrf_field() }}
+                                    <textarea class="form-control form-control-sm status" type="text" name="status" id="{{ $lead->id }}">{{ $lead->status }}</textarea>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     @endif
